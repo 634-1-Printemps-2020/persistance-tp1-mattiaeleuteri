cdd73ed8ee4c00580eb26e29c7742881fcc24842
Zen <mattia.eleuteri@gmail.com>
2020-02-20 08:51:41 +0100

    premiere version du tp
    creation Counter, UpperLimitedPositiveCounter et CounterException

--- src/main/java/ch.hesge.cours634.counter/Counter.java
+++ src/main/java/ch.hesge.cours634.counter/Counter.java
@@ -2,4 +2,22 @@ package ch.hesge.cours634.counter;
 
 public class Counter implements ICounter {
 
+    private int value;
+
+    public Counter(int value) {
+        this.value = value;
+    }
+
+    public void inc() throws CounterException {
+        this.value += 1;
+    }
+
+    public void add(int step) throws CounterException {
+        this.value += step;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
 }
--- src/main/java/ch.hesge.cours634.counter/UpperLimitedPositiveCounter.java
+++ src/main/java/ch.hesge.cours634.counter/UpperLimitedPositiveCounter.java
@@ -1,6 +1,34 @@
 package ch.hesge.cours634.counter;
 
-public class UpperLimitedPositiveCounter extends ICounter {
+public class UpperLimitedPositiveCounter extends Counter {
+
+    private int min = 0;
+    private int max;
+    private int value;
+
+    public UpperLimitedPositiveCounter(int min, int max, int value) {
+        super(value);
+        this.min = min;
+        this.max = max;
+    }
+
+    public void inc() throws CounterException {
+        this.value += 1;
+    }
+
+    public void add(int step) throws CounterException {
+        if (step < min){
+            throw new CounterException("Negative value is not allowed");
+        }
+        else if (step > max){
+            throw new CounterException("Value is over the maximum value allowed");
+        }
+        this.value += step;
+    }
+
+    public int getValue() {
+        return super.getValue();
+    }
 
 
 }
